fffd5cb42106680c8be35acda8bfcb98d0e3103e
sspieser <sspieser@gmail.com>
2017-04-05 21:39:20 +0200

    sauvegarde du local

---
+++ src/parser/GPXParserException.java
@@ -0,0 +1,16 @@
+package parser;
+
+public class GPXParserException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 * @param string
+	 */
+	public GPXParserException(String string) {
+		super(string);
+	}
+
+
+}
